979afda4284fc40a66251ce08bb03694963159e6
Dharrya <dharrya@gmail.com>
2013-08-19 21:13:53 +0300

    More selenium accessors for repos BasicItem

--- repos/BasicItem.php
+++ repos/BasicItem.php
@@ -13,4 +13,66 @@ class BasicItem
 		return \lib\Item\WebItem::byXPath($path);
 	}
 
+	/**
+	 * @param string $className
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementByClassName($className)
+	{
+		return \lib\Item\WebItem::byClassName($className);
+	}
+
+	/**
+	 * @param string $selector
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementByCssSelector($selector)
+	{
+		return \lib\Item\WebItem::byCssSelector($selector);
+	}
+
+	/**
+	 * @param string $id
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementById($id)
+	{
+		return \lib\Item\WebItem::byId($id);
+	}
+
+	/**
+	 * @param string $text
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementByLinkText($text)
+	{
+		return \lib\Item\WebItem::byLinkText($text);
+	}
+
+	/**
+	 * @param string $text
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementByPartialLinkText($text)
+	{
+		return \lib\Item\WebItem::byPartialLinkText($text);
+	}
+
+	/**
+	 * @param string $name
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementByName($name)
+	{
+		return \lib\Item\WebItem::byName($name);
+	}
+
+	/**
+	 * @param string $tag
+	 * @return \lib\Item\WebItem
+	 */
+	protected static function webElementByTag($tag)
+	{
+		return \lib\Item\WebItem::byTag($tag);
+	}
 }
